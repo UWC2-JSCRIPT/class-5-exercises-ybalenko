e8a04a9d7d53239254384a16ad72fa1708660d1b
Yulia Balenko <yulia@YuliaBookPro13.local>
2022-11-21 22:49:22 -0800

    Added 4 exercises from Class 5 Homework

--- dom-crud.js
+++ dom-crud.js
@@ -1,14 +1,23 @@
 // Create a new <a> element containing the text "Buy Now!" 
 // with an id of "cta" after the last <p>
 
-
+const newE = document.createElement('a')
+newE.setAttribute('id', 'cta')
+newE.innerText = 'Buy now!'
+//body.appendChild(newE);
+pE = document.querySelectorAll('body > main > p')[0];
+pE.insertAdjacentElement('afterend', newE);
 // Access (read) the data-color attribute of the <img>,
 // log to the console
-
+const img = document.getElementsByTagName('img')[0];
+const colorImg = img.getAttribute('data-color')
 
 // Update the third <li> item ("Turbocharged"), 
 // set the class name to "highlight"
-
+const newL = document.getElementsByTagName('li')[2];
+newL.setAttribute('class', 'highlight')
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+const removeL = document.querySelector("body > main > p");
+removeL.remove()
--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,24 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
 // When a user clicks the – element, the count should decrease by 1 on screen.
+
+let counter = 0;
+const pCounter = document.createElement('p')
+pCounter.innerHTML = counter;
+const btnPlus = document.createElement('button');
+btnPlus.innerHTML = '+';
+btnPlus.addEventListener('click', (e) => {
+    counter++;
+    pCounter.innerHTML = counter;
+})
+
+const btnMinus = document.createElement('button');
+btnMinus.innerHTML = '-';
+btnMinus.addEventListener('click', (e) => {
+    counter--;
+    pCounter.innerHTML = counter;
+})
+
+const body = document.getElementsByTagName('body')[0];
+body.appendChild(btnMinus);
+body.appendChild(btnPlus);
+body.appendChild(pCounter);
